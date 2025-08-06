d281d5aeb55926e445cc62a14a147ed3792176fa
Yasingokkaya <yasingokkaya91@gmail.com>
2025-08-07 00:21:06 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'ops-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'logo.png',
+  'logo-192.png',
+  'logo-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
